a8e4f782a9e27d6e5065e129137ff7916a4bcd71
Tom Briden <tom@decompile.me.uk>
2016-10-04 17:50:25 +0100

    Update config and module info

--- common/service.sh
+++ common/service.sh
@@ -2,3 +2,4 @@
 
 # This script will be executed in late_start service mode
 # More info in the main Magisk thread
+/system/bin/sshd
--- config.sh
+++ config.sh
@@ -36,7 +36,7 @@ CACHEMOD=false
 
 # This will be the folder name under /magisk or /cache/magisk
 # This should also be the same as the id in your module.prop to prevent confusion
-MODID=template
+MODID=angler_openssh
 
 # Set to true if you need automount
 # Most mods would like it to be enabled
@@ -49,7 +49,7 @@ POSTFS=false
 POSTFSDATA=false
 
 # Set to true if you need late_start service script (Only available in non-cache mods)
-LATESTARTSERVICE=false
+LATESTARTSERVICE=true
 
 ##########################################################################################
 # Installation Message
@@ -59,7 +59,7 @@ LATESTARTSERVICE=false
 
 print_modname() {
   ui_print "*******************************"
-  ui_print "     Magisk Module Template    "
+  ui_print "     OpenSSH Magisk Module     "
   ui_print "*******************************"
 }
 
@@ -99,7 +99,9 @@ set_permissions() {
   # Some templates if you have no idea what to do:
 
   # set_perm_recursive  <dirname>                <owner> <group> <dirpermission> <filepermission> <contexts> (default: u:object_r:system_file:s0)
-  # set_perm_recursive  $MODPATH/system/lib       0       0       0755            0644
+  set_perm_recursive  $MODPATH/system/bin       0       0       0755            0755
+  set_perm_recursive  $MODPATH/system/xbin      0       0       0755            0755
+  set_perm_recursive  $MODPATH/system/lib       0       0       0755            0644
 
   # set_perm  <filename>                         <owner> <group> <permission> <contexts> (default: u:object_r:system_file:s0)
   # set_perm  $MODPATH/system/bin/app_process32   0       2000    0755         u:object_r:zygote_exec:s0
